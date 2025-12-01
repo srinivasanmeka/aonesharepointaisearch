7374167ed9fb408de633df13c03d0484c200a179
srinivasanmeka <95310173+srinivasanmeka@users.noreply.github.com>
2025-12-01 14:44:09 -0500

    Add files via upload

---
+++ aoneSingleDocSynchtoAWSPGVector.py
@@ -0,0 +1,182 @@
+# AWS Lambda Function to Ingest single document aoneSingleDocSynchtoAWSPGVector
+# ITs a push/upload from sharepoint to postgres
+# 
+import json
+import base64
+import os
+import boto3
+import psycopg
+from pgvector.psycopg import register_vector
+from pypdf import PdfReader
+import io
+import uuid
+from datetime import datetime, timezone
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+AWS_REGION = "us-east-1"
+BEDROCK_EMBEDDING_MODEL = "cohere.embed-english-v3"
+SECRET_NAME = "rds-db-credentials/cluster-CZRGZG2DXJGCQGM4OZTEHRR2SU/postgres/1762906251764"
+
+
+# ----------------------------------------------------
+# Get PostgreSQL connection (pgvector enabled)
+# ----------------------------------------------------
+def get_db_connection():
+    sm = boto3.client("secretsmanager", region_name=AWS_REGION)
+    secret_dict = json.loads(sm.get_secret_value(SecretId=SECRET_NAME)["SecretString"])
+
+    conn = psycopg.connect(
+        host=secret_dict["host"],
+        dbname="aonebankpgvdb",
+        user=secret_dict["username"],
+        password=secret_dict["password"],
+        port=secret_dict["port"]
+    )
+    register_vector(conn)
+    return conn
+
+
+# ----------------------------------------------------
+# Extract PDF text from local aws tmp file
+# ----------------------------------------------------
+def extract_text_from_pdf(tmp_pdf_path):
+
+    try: 
+         reader = PdfReader(tmp_pdf_path)
+         text = ""
+         for page in reader.pages:
+             text += page.extract_text() or ""
+         return text
+    except Exception as e:
+        print(f"PDF extraction error: {e}")
+        return None
+
+# ----------------------------------------------------
+# Get embeddings for a list of chunks
+# ----------------------------------------------------
+def generate_embeddings(chunks):
+    bedrock = boto3.client("bedrock-runtime", region_name=AWS_REGION)
+
+    body = json.dumps({
+        "texts": chunks,
+        "input_type": "search_document"
+    })
+
+    response = bedrock.invoke_model(
+        modelId=BEDROCK_EMBEDDING_MODEL,
+        contentType="application/json",
+        accept="application/json",
+        body=body
+    )
+
+    data = json.loads(response["body"].read())
+    return data["embeddings"]
+
+
+# ----------------------------------------------------
+# Main Lambda Handler
+# ----------------------------------------------------
+def lambda_handler(event, context):
+
+    # API Gateway sends body as a string
+    if isinstance(event.get("body"), str):
+        body = json.loads(event["body"])
+    else:
+        body = event["body"]
+
+    file_name = body["filename"]
+    file_b64 = body["base64"]
+
+    # ----------------------------------------------------
+    # 1. Convert Base64 ? PDF
+    # ----------------------------------------------------
+    if not file_b64:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Missing base64 field"})
+        }
+
+    file_bytes = base64.b64decode(file_b64)
+
+    tmp_path = f"/tmp/{file_name}.pdf"
+    with open(tmp_path, "wb") as f:
+        f.write(file_bytes)
+
+    # ----------------------------------------------------
+    # 2. Extract Text
+    # ----------------------------------------------------
+    document_type = "pdf"
+    text = extract_text_from_pdf(tmp_path)
+
+    if not text.strip():
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "No text extracted from PDF"})
+        }
+
+    # ----------------------------------------------------
+    # 3. Chunk Text
+    # ----------------------------------------------------
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1500,
+        chunk_overlap=200
+    )
+    chunks = splitter.split_text(text)
+
+    # ----------------------------------------------------
+    # 4. Generate Embeddings
+    # ----------------------------------------------------
+    embeddings = generate_embeddings(chunks)
+
+    # ----------------------------------------------------
+    # 5. create connection to postgres/ pgvector db document chunks and embeddings
+    # ----------------------------------------------------
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    try:
+        # delete from sprepo_docs table, if document is already uploaded, so new version is stored in vector db
+        cur.execute(
+            "DELETE FROM aone_corp_sprepo_docs WHERE document_name = %s",
+            (file_name,)
+        )
+
+        deleted_rows = cur.rowcount
+
+        if deleted_rows == 0:
+            print(f"[TRACE] New file upload: {file_name}")
+        else:
+            print(f"[TRACE] Replacing file: {file_name} | Deleted: {deleted_rows}")
+
+        # 
+        for chunk, emb in zip(chunks, embeddings):
+            chunk_id = str(uuid.uuid4())
+
+            cur.execute(
+                """
+                INSERT INTO aone_corp_sprepo_docs 
+                (id, document_name, document_type, chunk_text, embedding) 
+                VALUES (%s, %s, %s, %s, %s)
+                """,
+                (chunk_id, file_name, document_type, chunk, emb)
+            )
+
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print("[ERROR] Upload failed:", str(e))
+        raise
+
+    finally:
+        cur.close()
+        conn.close()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "PDF processed and ingested",
+            "chunks": len(chunks)
+        })
+    }
